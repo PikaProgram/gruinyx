7ada14cecfe47e6018fb398570f7f76c8ac67d8c
Rasyad Satyatma <rasyad.satyatma01@gmail.com>
2026-01-18 19:40:44 +0700

    server/feat: elysia logging via logixlysia

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,12 @@
 import { Elysia } from "elysia";
 import { openapi } from '@elysiajs/openapi';
+import logixlysia from "logixlysia";
 
 const app = new Elysia();
 
-app.use(openapi()); 
+app.use(openapi());
+
+app.use(logixlysia());
 
 app.listen(process.env["SERVER_PORT"] || 3000, () => {
   console.log(`Server is running on port ${process.env["SERVER_PORT"] || 3000}`);
